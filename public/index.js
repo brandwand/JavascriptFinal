adb01f0a9209540eced104d3413db363820cf7af
brandwand <brandwand@yahoo.com>
2017-03-08 12:12:33 -0700

    Added some changes

---
+++ public/index.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+var bcrypt = require('bcrypt-nodejs'),
+pug = require('pug'),
+path = require('path'),
+menu = require('./menu.json');
+var hash;
+
+bcrypt.hash("bacon", null, null, function (err, hash) {
+    // Store hash in your password DB.
+    console.log(hash);
+    bcrypt.compare("bacon", hash, function (err, res) {
+        console.log(res);
+        // res == true
+    });
+    bcrypt.compare("veggies", hash, function (err, res) {
+        // res = false
+        console.log(res);
+    });
+});
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', function(req, res){
+    res.render('index');
+} );
+
+
+ app.get('/:viewname', function(req, res){
+     res.render(req.params.viewname, menu);
+ });
+
+
+app.get('/:viewname', function(req, res){
+    res.render(req.params.viewname, menu);
+});
+
+
+
+app.listen(3000);
