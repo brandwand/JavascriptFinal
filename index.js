dfb2838735e0509c0d1533835557f988420ac36e
brandwand <brandwand@yahoo.com>
2017-03-13 08:41:08 -0600

    Added the mongo stuff and basic creation of Users

--- index.js
+++ index.js
@@ -32,7 +32,13 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
-
+var urlencodedParser = bodyParser.urlencoded({ extended: true});
+app.get('/create', route.create);
+app.get('/edit/:id', route.edit);
+app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createPerson);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/delete/:id', route.delete);
 app.get('/', function(req, res){
     res.render('index');
 } );
